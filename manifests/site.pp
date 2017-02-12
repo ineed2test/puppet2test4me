0d84c72f24c9ace795cc1936e16c54b77e5777a4
ineed2test <ineed2test4me@gmail.com>
2017-02-12 17:24:01 +0100

    Create site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,13 @@
+file {'/tmp/example-ip':                                            # resource type file and filename
+  ensure  => present,                                               # make sure it exists
+  mode    => 0644,                                                  # file permissions
+  content => "Here is my Public IP Address: ${ipaddress_eth0}.\n",  # note the ipaddress_eth0 fact
+}
+
+node 'deb02.localdomain' {
+  class { 'apache': }             # use apache module
+  apache::vhost { 'example.com':  # define vhost resource
+    port    => '80',
+    docroot => '/var/www/html'
+  }
+}
