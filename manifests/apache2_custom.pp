dd6f034d86a0bb00e8ccc63caae376e8cc676757
ineed2test <ineed2test4me@gmail.com>
2017-02-26 23:02:37 +0100

    Update apache2_custom.pp

--- manifests/apache2_custom.pp
+++ manifests/apache2_custom.pp
@@ -4,21 +4,29 @@ class { 'apache':}
 #}
 
 # Add vhost File
-class apache::vhosts {
-        
-  if $::osfamily == 'RedHat' {
-    file { '/etc/httpd/conf.d/vhost.conf':
-      ensure    => file,
-      content   => template('apache/vhosts-rh.conf.erb'),
-    }
-  } elsif $::osfamily == 'Debian' {
-    file { "/etc/apache2/sites-available/$servername.conf":
-      ensure  => file,
-      content  => template('/etc/puppet/environment/staging/files/apache/firstvhost.conf.erb'),
-    }
-  } else {
-    print "This is not a supported distro."
-  }
-        
-}
+#class apache::vhosts {
+#        
+#  if $::osfamily == 'RedHat' {
+#    file { '/etc/httpd/conf.d/vhost.conf':
+#      ensure    => file,
+#      content   => template('apache/vhosts-rh.conf.erb'),
+#    }
+#  } elsif $::osfamily == 'Debian' {
+#    file { "/etc/apache2/sites-available/$servername.conf":
+#      ensure  => file,
+#      content  => template('/etc/puppet/environment/staging/files/apache/firstvhost.conf.erb'),
+#    }
+#  } else {
+#    print "This is not a supported distro."
+# }
+#        
+#}
 
+class apache::vhost {
+  file { '/etc/apache2/sites-available/$servername.conf':
+    content => template('/etc/puppet/environment/staging/files/apache/firstvhost.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+}
