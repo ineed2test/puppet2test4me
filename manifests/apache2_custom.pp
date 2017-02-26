be9239a103ce263fc46ec69c4b1f0a4d0a838ff7
ineed2test <ineed2test4me@gmail.com>
2017-02-26 22:07:35 +0100

    first vhost from template file

--- manifests/apache2_custom.pp
+++ manifests/apache2_custom.pp
@@ -1,4 +1,24 @@
 class { 'apache':}
-apache::vhost { 'blahexample.com': # define vhost resource
-docroot => '/var/www/html'
+#apache::vhost { 'blahexample.com': # define vhost resource
+#docroot => '/var/www/html'
+#}
+
+# Add vhost File
+class apache::vhosts {
+        
+  if $::osfamily == 'RedHat' {
+    file { '/etc/httpd/conf.d/vhost.conf':
+      ensure    => file,
+      content   => template('apache/vhosts-rh.conf.erb'),
+    }
+  } elsif $::osfamily == 'Debian' {
+    file { "/etc/apache2/sites-available/$servername.conf":
+      ensure  => file,
+      content  => template('files/apache/firstvhost.conf.erb'),
+    }
+  } else {
+    print "This is not a supported distro."
+  }
+        
 }
+
