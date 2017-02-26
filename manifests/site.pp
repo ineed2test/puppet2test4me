850477a57748ba6d72ef63845d2be4bc472a838a
ineed2test <ineed2test4me@gmail.com>
2017-02-26 17:15:15 +0100

    test without class apache

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,10 +4,10 @@ file {'/tmp/example-ip':                                            # resource t
   content => "Here is my Public IP Address: ${ipaddress_eth0}.\n",  # note the ipaddress_eth0 fact
 }
 
-node 'deb02.localdomain' {
-  class { 'apache': }             # use apache module
-  apache::vhost { 'example.com':  # define vhost resource
-    port    => '80',
-    docroot => '/var/www/html'
-  }
-}
+#node 'deb02.localdomain' {
+#  class { 'apache': }             # use apache module
+#  apache::vhost { 'example.com':  # define vhost resource
+#    port    => '80',
+#    docroot => '/var/www/html'
+#  }
+#}
