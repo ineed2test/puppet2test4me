dd4d7f26b4662c08a0af168ce10c6fcd21eab607
ineed2test <ineed2test4me@gmail.com>
2017-03-30 16:10:49 +0200

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,14 +4,9 @@ node "deb02.localdomain" {
 class { 'apt':
 update => { 'frequency' => 'always' },
 }
-exec { 'apt-get-update':
-command     => '/usr/bin/apt-get update',
-refreshonly => true,
-}
 
 $packages = ["libapache2-modsecurity"]
 package {
 $packages: ensure => installed,
-require    => Exec['apt-get-update'],
 }
 }
