8f2f0debcddf51eb75d7f97c1b32f7f17759f6c4
ineed2test <ineed2test4me@gmail.com>
2017-04-06 21:15:03 +0200

    Update create_ssh_user.pp

--- manifests/create_ssh_user.pp
+++ manifests/create_ssh_user.pp
@@ -1,15 +1,15 @@
      group { 'web':
   			  ensure => 'present',
-  			  gid    => '502',
+  			  gid    => '1502',
      }
 
 user { 'jargyle':
  			  ensure           => 'present',
-       gid              => '1501',
+       gid              => '1502',
        home             => '/home/jargyle',
        password         => '!!',
        password_max_age => '99999',
        password_min_age => '0',
        shell            => '/bin/bash',
-       uid              => '1502',
+       uid              => '1501',
      }
