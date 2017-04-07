392457ca7f97e172d590acb2dbcbe5542772faca
ineed2test <ineed2test4me@gmail.com>
2017-04-07 12:50:48 +0200

    Create init.pp

---
+++ modules/mymotd/manifests/init.pp
@@ -0,0 +1,13 @@
+class mymotd {
+ 
+  package { 'figlet':
+    ensure => 'installed'
+  }
+  
+  file { '/etc/motd':
+    ensure => 'present',
+    source => 'puppet:///modules/mymotd/mymotd',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755'
+  }
