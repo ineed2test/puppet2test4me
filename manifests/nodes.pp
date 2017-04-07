915d89db40bbc70c37ed8edb1c5743d5773c465d
ineed2test <ineed2test4me@gmail.com>
2017-04-07 20:58:06 +0200

    Update nodes.pp

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -3,6 +3,7 @@
 node 'deb02.localdomain' {
       include apache
       class { 'mymotd': }
+      include hiera_users
 
 #      include git
 #      include apt     !!! notworking together with class 'apt'
