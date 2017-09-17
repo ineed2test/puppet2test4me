7a441a951d76e08dca603009396aa7568a222959
ulinux <ulinux@ulinux>
2017-09-17 17:56:43 +0200

    add mod_jk test pp.file v8

--- manifests/apache2_mod_jk.pp
+++ manifests/apache2_mod_jk.pp
@@ -1,7 +1,7 @@
-class { '::apache::mod::jk':
-  ip => '192.168.2.15',
-  workers_file => '/tmp/workers.properties',
-  mount_file => '/tmp/uriworkermap.properties',
-  shm_file => '/tmp/jk.shm',
-  shm_size => '50M',
-}
+#class { '::apache::mod::jk':
+#  ip => '192.168.2.15',
+#  workers_file => '/tmp/workers.properties',
+#  mount_file => '/tmp/uriworkermap.properties',
+#  shm_file => '/tmp/jk.shm',
+#  shm_size => '50M',
+#}
