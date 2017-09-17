c3676948800961e7d1b120392043bed6b203dab1
ulinux <ulinux@ulinux>
2017-09-17 16:48:16 +0200

    add mod_jk v15 test pp.file

---
+++ manifests/apache2_mod_jk.pp
@@ -0,0 +1,10 @@
+class { '::apache::mod::jk':
+  ip           = '192.168.2.15',
+  workers_file = 'conf/workers.properties',
+  mount_file   = 'conf/uriworkermap.properties',
+  shm_file     = 'run/jk.shm',
+  shm_size     = '50M',
+  $workers_file_content = {
+    <Content>
+  },
+}
