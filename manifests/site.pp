bc2f62ae9797fb7071243dced8c35de6e44bd4e3
ulinux <ulinux@ulinux>
2017-09-18 14:50:18 +0200

    add mod_jk test pp.file v35

--- manifests/site.pp
+++ manifests/site.pp
@@ -71,8 +71,8 @@ create_resources('haproxy::listen', $haproxy_listen)
 $apache_vhost = hiera('apache::vhost', {})
 create_resources('apache::vhost', $apache_vhost)
 
-$apache_mod_jk = hiera('apache::mod::jk', {})
-create_resources('apache::mod::jk', $apache_mod_jk)
+#$apache_mod_jk = hiera('apache::mod::jk', {})
+#create_resources('apache::mod::jk', $apache_mod_jk)
 
 #package
 $packages = hiera('packages', {})
