266a8b1426c2b8d99a6b28b3162ccc98dd868d20
ulinux <ulinux@ulinux>
2017-09-20 13:01:26 +0200

    diverse apache settings v19 /etc/hosts settings

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,7 +79,7 @@ $packages = hiera('packages', {})
 create_resources('package', $packages)
 
 # host /etc/hosts
-$hosts = hiera_hash('hosts', {})
+$hosts = hiera('hosts', {})
 create_resources('host', $hosts)
 
 #$apache_mod_jk = hiera('apache::mod', {})
