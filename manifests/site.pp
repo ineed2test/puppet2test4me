f091f6a2a54fc0a6e9e2e542dc8fc7264b1dfeba
ulinux <ulinux@ulinux>
2017-09-20 14:19:53 +0200

    4

--- manifests/site.pp
+++ manifests/site.pp
@@ -79,8 +79,8 @@ $packages = hiera('packages', {})
 create_resources('package', $packages)
 
 # host /etc/hosts
-$hosts = hiera('hosts', {})
-create_resources('host', $hosts)
+$host = hiera('host', {})
+create_resources('host', $host)
 
 #$apache_mod_jk = hiera('apache::mod', {})
 #create_resources('apache::mod', $apache_mod_jk)
