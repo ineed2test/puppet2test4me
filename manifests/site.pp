ad7ef7dfbe4ff24847d4c8fd9895c025eb113b51
ulinux <ulinux@ulinux>
2017-09-24 14:56:01 +0200

    puppet ordering v10

--- manifests/site.pp
+++ manifests/site.pp
@@ -116,4 +116,4 @@ create_resources('exec', $execs)
 #ordering with collectors
 
 #package <| |> -> file['/home/kundea'] <| |> -> tomcat <| |> -> file['/home/kundea/tomcat/conf/server.xml']
-Package['openjdk-8-jdk'] -> File['/home/kundea']
+Package['openjdk-8-jdk'] -> File['/home/kundea'] -> Tomcat_install['/home/kundea/tomcat'] -> File['/home/kundea/tomcat/conf/server.xml']
