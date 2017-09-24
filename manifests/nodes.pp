5a2617dfee19a9abdf0b44ae10d68026233161b8
ulinux <ulinux@ulinux>
2017-09-24 17:10:12 +0200

    puppet ordering v20

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -48,6 +48,6 @@ node 'deb03.localdomain' {
     owner => 'kundea',
     group => 'kundea',
     mode => 'u=rwx,g=rw,o=rw',
-    before => Class['tomcat::install']
+    before => tomcat::install['/home/kundea/tomcat']
     }
   }
