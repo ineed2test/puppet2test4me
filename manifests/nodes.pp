417ac0082eaa1a3278c4f3169021fd3da1a2ec97
ulinux <ulinux@ulinux>
2017-09-25 17:26:24 +0200

    puppet ordering v66

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -49,7 +49,7 @@ node 'deb03.localdomain' {
     group => 'kundea',
     mode => 'u=rwx,g=rw,o=rw',
 #    require => File['/home/kundea'],
-    require => Class['tomcat'],
+    before => Class['tomcat'],
     ensure => file
     }
    }
