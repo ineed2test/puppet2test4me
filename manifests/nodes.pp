980cecf64cd506747e7a94aacc49a0840e717027
ulinux <ulinux@ulinux>
2017-09-25 10:18:14 +0200

    puppet ordering v45

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -37,16 +37,16 @@
 
 
 #}
-node default {}
-node 'deb03.localdomain' {
-  file {'/home/kundea/tomcat/conf/server.xml':
-## template function
-## apache/vhost.conf.erb will load the file <MODULES DIRECTORY>/apache/templates/vhost.conf.erb
-##  https://docs.puppet.com/puppet/latest/function.html#template
-    content => template('kundea/server.xml'),
-#    source => 'puppet:///modules/kundea/server.xml.erb',
-    owner => 'kundea',
-    group => 'kundea',
-    mode => 'u=rwx,g=rw,o=rw',
-    }
-   }
+#node default {}
+#node 'deb03.localdomain' {
+#  file {'/home/kundea/tomcat/conf/server.xml':
+### template function
+### apache/vhost.conf.erb will load the file <MODULES DIRECTORY>/apache/templates/vhost.conf.erb
+###  https://docs.puppet.com/puppet/latest/function.html#template
+#    content => template('kundea/server.xml'),
+##    source => 'puppet:///modules/kundea/server.xml.erb',
+#    owner => 'kundea',
+#    group => 'kundea',
+#    mode => 'u=rwx,g=rw,o=rw',
+#    }
+#   }
