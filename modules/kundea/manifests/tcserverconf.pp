8d77eba662fa18b96a8867839cb518ede4d33425
ulinux <ulinux@ulinux>
2017-09-26 20:51:27 +0200

    v160

---
+++ modules/kundea/manifests/tcserverconf.pp
@@ -0,0 +1,48 @@
+#
+
+#class kundea::webserver {
+#  file { '/home/kundeauser/tomcat/conf/server.xml':
+#    content => template('kundea/webserver/server.xml.erb'),
+#    owner   => 'kundeauser',
+#    group   => 'kundeauser',
+#    mode    => '0600',
+#  }
+#}
+
+#class kundea::webserverfile {
+#  file {'/home/kundea/tomcat/conf/server.xml':
+#   ### template function
+#   ### apache/vhost.conf.erb will load the file <MODULES DIRECTORY>/apache/templates/vhost.conf.erb
+#   ###  https://docs.puppet.com/puppet/latest/function.html#template
+#    content => template('kundea/server.xml'),
+##   ##    source => 'puppet:///modules/kundea/server.xml.erb',
+#    owner => 'kundea',
+#    group => 'kundea',
+#    mode => 'u=rwx,g=rw,o=rw',
+#   #   require Definition wird nach der angegebenen Ressource angewendet
+#    require => [ Class['tomcat'], File['/home/kundea'] ],
+#    ensure => file,
+#    }
+#}
+
+
+define kundea::tcserverconf (
+  $owner = undef,
+  $group = undef,
+  ) {
+  file {$title:
+## template function
+### apache/vhost.conf.erb will load the file <MODULES DIRECTORY>/apache/templates/vhost.conf.erb
+##  https://docs.puppet.com/puppet/latest/function.html#template
+    content => template('kundea/server.xml'),
+#    source => 'puppet:///modules/kundea/server.xml.erb',
+    owner => $owner,
+    group => $group,
+    mode => 'u=rwx,g=rw,o=rw',
+#    require => File['/home/kundea'],
+#   require Definition wird nach der angegebenen Ressource angewendet
+#    require => [ Class['tomcat'], File['/home/kundea'] ],
+#    ensure => file,
+#    tag => [try]
+    }
+   }
